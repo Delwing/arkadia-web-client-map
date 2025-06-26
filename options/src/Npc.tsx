dc76e6abaf66e3754960e4784d47e8ae6cd19d9b
Piotr Wilczyński <delwing@gmail.com>
2025-06-26 20:58:30 +0200

    update settings page

--- options/src/Npc.tsx
+++ options/src/Npc.tsx
@@ -36,10 +36,10 @@ function Npc() {
             <div className={'m-2'}>
                 <button className={'btn btn-primary btn-sm mb-2 mr-2'} onClick={() => downloadNpcs()}>Pobierz bazę</button>
                 <button className={'btn btn-error btn-sm mb-2'} onClick={() => clearNpcs()}>Wyczyść bazę</button>
-                <table className="min-w-full border border-gray-700 text-sm">
+                <table className="min-w-full border border-gray-700 text-sm table-zebra">
                     <tbody>
                     {npcs.sort((a, b) => a.name.localeCompare(b.name)).map((item) => (
-                        <tr key={item.name + '-' + item.loc} className="odd:bg-gray-800 even:bg-gray-700">
+                        <tr key={item.name + '-' + item.loc}>
                             <td className="px-2 py-1 border border-gray-700">{item.name}</td>
                             <td className="px-2 py-1 border border-gray-700">{item.loc}</td>
                         </tr>))}
