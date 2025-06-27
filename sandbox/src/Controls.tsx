026e389a8a5e14820c0e8862f23ed995101418e6
Piotr <delwing@gmail.com>
2025-06-27 13:00:22 +0200

    Move reset button to side and unify styles

--- sandbox/src/Controls.tsx
+++ sandbox/src/Controls.tsx
@@ -5,14 +5,11 @@ export function Controls() {
     return (
         <>
             {createPortal(
-                <Button size="sm" variant="secondary" onClick={() => window.Output.clear()}>Reset</Button>,
-                document.getElementById('controls')!
-            )}
-            {createPortal(
                 <div className="d-flex flex-column gap-2">
-                    <Button size="sm" variant="primary">Button 1</Button>
-                    <Button size="sm" variant="primary">Button 2</Button>
-                    <Button size="sm" variant="primary">Button 3</Button>
+                    <Button size="sm" variant="secondary" onClick={() => window.Output.clear()}>Reset</Button>
+                    <Button size="sm" variant="secondary">Button 1</Button>
+                    <Button size="sm" variant="secondary">Button 2</Button>
+                    <Button size="sm" variant="secondary">Button 3</Button>
                 </div>,
                 document.getElementById('sandbox-buttons')!
             )}
