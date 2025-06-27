5b9efb2cb0e5033fb1e9156d624908dfba50a245
Piotr <delwing@gmail.com>
2025-06-27 12:48:03 +0200

    Add sandbox side buttons via React portal

--- sandbox/src/Controls.tsx
+++ sandbox/src/Controls.tsx
@@ -2,11 +2,20 @@ import {createPortal} from "react-dom";
 import {Button} from "react-bootstrap";
 
 export function Controls() {
-
-    return <>
-        {createPortal(
-            <Button size={"sm"} variant={'secondary'} onClick={() => window.Output.clear()}>Reset</Button>,
-            document.getElementById('controls')!
-        )}
-    </>
+    return (
+        <>
+            {createPortal(
+                <Button size="sm" variant="secondary" onClick={() => window.Output.clear()}>Reset</Button>,
+                document.getElementById('controls')!
+            )}
+            {createPortal(
+                <div className="d-flex flex-column gap-2">
+                    <Button size="sm" variant="primary">Button 1</Button>
+                    <Button size="sm" variant="primary">Button 2</Button>
+                    <Button size="sm" variant="primary">Button 3</Button>
+                </div>,
+                document.getElementById('sandbox-buttons')!
+            )}
+        </>
+    );
 }
