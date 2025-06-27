cbd13d86a57cebd94372bc748f06358d9e4d157f
Piotr <delwing@gmail.com>
2025-06-27 14:08:09 +0200

    Add trigger tester modal to sandbox

--- sandbox/src/Controls.tsx
+++ sandbox/src/Controls.tsx
@@ -1,18 +1,30 @@
 import {createPortal} from "react-dom";
-import {Button} from "react-bootstrap";
+import {Button, Modal} from "react-bootstrap";
+import {useState} from "react";
+import TriggerTester from "./TriggerTester.tsx";
+
 
 export function Controls() {
+    const [showTester, setShowTester] = useState(false);
     return (
         <>
             {createPortal(
                 <div className="d-flex flex-column gap-2">
                     <Button size="sm" variant="secondary" onClick={() => window.Output.clear()}>Reset</Button>
-                    <Button size="sm" variant="secondary">Button 1</Button>
+                    <Button size="sm" variant="secondary" onClick={() => setShowTester(true)}>Trigger Tester</Button>
                     <Button size="sm" variant="secondary">Button 2</Button>
                     <Button size="sm" variant="secondary">Button 3</Button>
                 </div>,
-                document.getElementById('sandbox-buttons')!
+                document.getElementById('sandbox-buttons')!,
             )}
+            <Modal show={showTester} onHide={() => setShowTester(false)} size="lg">
+                <Modal.Header closeButton>
+                    <Modal.Title>Trigger Tester</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <TriggerTester />
+                </Modal.Body>
+            </Modal>
         </>
     );
 }
