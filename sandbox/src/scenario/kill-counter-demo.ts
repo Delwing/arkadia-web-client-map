555673154834113a28891f2f6017c1b10105752c
Piotr <delwing@gmail.com>
2025-06-28 16:51:38 +0200

    Expand kill counter sandbox

--- sandbox/src/scenario/kill-counter-demo.ts
+++ sandbox/src/scenario/kill-counter-demo.ts
@@ -3,15 +3,17 @@ import { fakeClient } from "../index.ts";
 
 export default new ClientScript(fakeClient)
     .reset()
-    .fake("Zabiles wielkiego dzikiego kamiennego trolla.")
-    .fake("Zabiles poteznego wscieklego krasnoluda chaosu.")
-    .fake("Zabiles malutkiego wscieklego snotlinga.")
-    .fake("Zabiles brzydkiego zgarbionego goblina.")
-    .fake("Zabiles dzikiego wscieklego trolla.")
-    .fake("Zabiles ogromnego szybkiego kamiennego trolla.")
-    .fake("Zabiles dzikiego roslego krasnoluda chaosu.")
-    .fake("Zabiles malego przebieglego goblina.")
-    .fake("Zabiles silnego roslego trolla.")
-    .fake("Zabiles Ryszard.")
+    // moje zabicia
+    .fake("Zabiles poteznego smoka chaosu.")
+    .fake("Zabiles malego smoka chaosu.")
+    .fake("Zabiles silnego kamiennego trolla.")
+    .fake("Zabiles poteznego kamiennego trolla.")
+    .fake("Zabiles wscieklego krasnoluda chaosu.")
+    .fake("Zabiles Helga.")
+    // zabicia druzyny
+    .fake("> Eamon zabil smoka chaosu.")
+    .fake("> Beata zabila smoka chaosu.")
+    .fake("> Eamon zabil poteznego kamiennego trolla.")
+    .fake("> Beata zabila wscieklego krasnoluda chaosu.")
     .send("/zabici");
 
