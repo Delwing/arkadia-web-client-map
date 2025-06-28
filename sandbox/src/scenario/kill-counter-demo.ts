22fc3f7a03d5afc18aedc926fd564680560e9b42
Piotr Wilczynski <delwing@gmail.com>
2025-06-28 20:06:05 +0200

    kill counter demo drop prompt

--- sandbox/src/scenario/kill-counter-demo.ts
+++ sandbox/src/scenario/kill-counter-demo.ts
@@ -3,18 +3,16 @@ import { fakeClient } from "../index.ts";
 
 export default new ClientScript(fakeClient)
     .reset()
-    // moje zabicia
     .fake("Zabiles poteznego smoka chaosu.")
     .fake("Zabiles malego smoka chaosu.")
     .fake("Zabiles silnego kamiennego trolla.")
     .fake("Zabiles poteznego kamiennego trolla.")
     .fake("Zabiles wscieklego krasnoluda chaosu.")
-    .fake("Zabiles Helga.")
-    // zabicia druzyny
-    .fake("> Eamon zabil smoka chaosu.")
-    .fake("> Beata zabila smoka chaosu.")
-    .fake("> Eamon zabil poteznego kamiennego trolla.")
-    .fake("> Beata zabila wscieklego krasnoluda chaosu.")
-    .fake("> Sindarion zabil barczystego jasnowlosego bykocentaura.")
+    .fake("Zabiles Ryszarda.")
+    .fake("Eamon zabil smoka chaosu.")
+    .fake("Beata zabila smoka chaosu.")
+    .fake("Eamon zabil poteznego kamiennego trolla.")
+    .fake("Beata zabila wscieklego krasnoluda chaosu.")
+    .fake("Sindarion zabil barczystego jasnowlosego bykocentaura.")
     .send("/zabici");
 
