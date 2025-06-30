1cb2bb986a694d66cf9c551c8485e9bdbe717036
Piotr Wilczyński <delwing@gmail.com>
2025-06-30 03:25:00 +0200

    lvl calc more real

--- sandbox/src/scenario/lvl-calc-demo.ts
+++ sandbox/src/scenario/lvl-calc-demo.ts
@@ -1,13 +1,14 @@
 import ClientScript from "../ClientScript.ts";
 import {fakeClient} from "../fakeClient.ts";
+import {color, findClosestColor} from "@client/src/Colors.ts";
 
 export default new ClientScript(fakeClient)
     .reset()
     .send("cechy")
-    .fake("Jestes potezny i duzo ci brakuje, zebys mogl wyzej ocenic swa sile.")
-    .fake("Jestes zreczny i bardzo duzo ci brakuje, zebys mogl wyzej ocenic swa zrecznosc.")
-    .fake("Jestes muskularny i troche ci brakuje, zebys mogl wyzej ocenic swa wytrzymalosc.")
-    .fake("Jestes inteligentny i duzo ci brakuje, zebys mogl wyzej ocenic swoj intelekt.")
-    .fake("Jestes odwazny i bardzo niewiele ci brakuje, zebys mogl wyzej ocenic swa odwage.")
-    .fake("Obecnie do waznych cech zaliczasz sile oraz wytrzymalosc, do srednio waznych zrecznosc, a do nieistotnych intelekt oraz odwage.");
+    .fake(color(findClosestColor("#949494")) + "Jestes potezny i duzo ci brakuje, zebys mogl wyzej ocenic swa sile.")
+    .fake(color(findClosestColor("#949494")) + "Jestes zreczny i bardzo duzo ci brakuje, zebys mogl wyzej ocenic swa zrecznosc.")
+    .fake(color(findClosestColor("#949494")) + "Jestes muskularny i troche ci brakuje, zebys mogl wyzej ocenic swa wytrzymalosc.")
+    .fake(color(findClosestColor("#949494")) + "Jestes inteligentny i duzo ci brakuje, zebys mogl wyzej ocenic swoj intelekt.")
+    .fake(color(findClosestColor("#949494")) + "Jestes odwazny i bardzo niewiele ci brakuje, zebys mogl wyzej ocenic swa odwage.")
+    .fake(color(findClosestColor("#949494")) + "Obecnie do waznych cech zaliczasz sile oraz wytrzymalosc, do srednio waznych zrecznosc, a do nieistotnych intelekt oraz odwage.");
 
