bfbc5cb4b9943e7d240c1c313728ea6597480893
Piotr <delwing@gmail.com>
2025-06-30 19:02:37 +0200

    Add settings file import/export (#87)
    * Add settings file import/export
    
    * Improve settings file upload UI
    
    * Display settings after import

--- options/src/App.tsx
+++ options/src/App.tsx
@@ -2,10 +2,11 @@ import './App.css'
 import {useState} from "react";
 import SettingsForm from "./Settings.tsx";
 import Npc from "./Npc.tsx";
+import SettingsFile from "./SettingsFile";
 
 
 function App() {
-    const [tab, setTab] = useState<'settings' | 'npc'>('settings')
+    const [tab, setTab] = useState<'settings' | 'npc' | 'file'>('settings')
 
     return (
         <div className="p-2">
@@ -22,9 +23,16 @@ function App() {
                 >
                     Odbiorcy paczek
                 </button>
+                <button
+                    className={`cursor-pointer px-4 py-2 font-medium ${tab === 'file' ? 'border-b-2 border-blue-500' : ''}`}
+                    onClick={() => setTab('file')}
+                >
+                    Plik ustawień
+                </button>
             </div>
             {tab === 'settings' && <SettingsForm />}
             {tab === 'npc' && <Npc />}
+            {tab === 'file' && <SettingsFile />}
         </div>
     )
 }
