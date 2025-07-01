2b417b3617165501b0f23f8910f4407696281e53
Piotr <delwing@gmail.com>
2025-07-01 11:31:38 +0200

    Update gates bind

---
+++ client/src/scripts/gates.ts
@@ -0,0 +1,26 @@
+import Client from "../Client";
+import { FunctionalBind } from "./functionalBind";
+
+export default function initGates(client: Client) {
+    const bind = new FunctionalBind(client, { key: "F2", ctrl: true, label: "CTRL+F2" });
+    const knock = () => {
+        Input.send("zastukaj we wrota");
+    };
+    bind.set(null, knock);
+
+    const showMessage = () => {
+        bind.set("zastukaj we wrota", knock);
+        return undefined;
+    };
+
+    const patterns = [
+        /^Probujesz otworzyc .*wrota.*/,
+        /^Probujesz otworzyc .*drzwiczki.*/,
+        /^Probujesz otworzyc .*krate.*/,
+        /^Probujesz otworzyc .*brame.*/,
+        /^Probujesz otworzyc niewielka furtke.*/,
+    ];
+
+    patterns.forEach(p => client.Triggers.registerTrigger(p, showMessage, "gates"));
+}
+
