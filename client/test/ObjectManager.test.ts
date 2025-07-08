34dba2d2ce4d787d1f7e2ca2bd053880ac6ce5aa
Piotr <delwing@gmail.com>
2025-07-08 23:01:55 +0200

    feat(web-client): add object list display

--- client/test/ObjectManager.test.ts
+++ client/test/ObjectManager.test.ts
@@ -28,7 +28,7 @@ describe('ObjectManager', () => {
     client.sendEvent('gmcp.objects.data', {
       '1': { desc: 'Goblin', hp: 5, attack_target: true, avatar_target: true },
     });
-    client.sendEvent('gmcp.object.nums', ['1']);
+    client.sendEvent('gmcp.objects.nums', ['1']);
     expect(manager.getObjectsOnLocation()).toEqual([
       { num: '1', desc: 'Goblin', state: 5, attack_target: true, avatar_target: true },
     ]);
@@ -38,7 +38,7 @@ describe('ObjectManager', () => {
     client.sendEvent('gmcp.objects.data', {
       '2': { desc: 'Orc', hp: 10 },
     });
-    client.sendEvent('gmcp.object.nums', { nums: [2] });
+    client.sendEvent('gmcp.objects.nums', { nums: [2] });
     expect(manager.getObjectsOnLocation()).toEqual([
       { num: '2', desc: 'Orc', state: 10, attack_target: undefined, avatar_target: undefined },
     ]);
@@ -47,7 +47,7 @@ describe('ObjectManager', () => {
   test('includes player from char info and state', () => {
     client.sendEvent('gmcp.char.info', { object_num: 99, name: 'Hero' });
     client.sendEvent('gmcp.char.state', { hp: 50 });
-    client.sendEvent('gmcp.object.nums', []);
+    client.sendEvent('gmcp.objects.nums', []);
     expect(manager.getObjectsOnLocation()).toEqual([
       { num: '99', desc: 'Hero', state: 50, attack_target: undefined, avatar_target: undefined },
     ]);
@@ -55,7 +55,7 @@ describe('ObjectManager', () => {
 
   test('sets avatar target flag', () => {
     client.sendEvent('gmcp.objects.data', { '1': { desc: 'Ogre', avatar_target: true } });
-    client.sendEvent('gmcp.object.nums', ['1']);
+    client.sendEvent('gmcp.objects.nums', ['1']);
     expect(manager.getObjectsOnLocation()).toEqual([
       { num: '1', desc: 'Ogre', state: undefined, attack_target: undefined, avatar_target: true },
     ]);
