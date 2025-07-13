c5363fd9b9448cc2f7c36c2b36c47c5d2d0275a4
Piotr Wilczynski <delwing@gmail.com>
2025-07-13 02:45:22 +0200

    Revert "fix auto login after reconnect (#263)"
    This reverts commit 6fcfeb65930fa337649bbdbe0c0c58aa0b484c7a.

--- web-client/src/ArkadiaClient.ts
+++ web-client/src/ArkadiaClient.ts
@@ -86,9 +86,9 @@ class ArkadiaClient {
                 this.emit('open', event);
                 this.emit('client.connect');
                 if (!this.lastConnectManual && this.userCommand && this.passwordCommand) {
-                    this.sendRaw(this.userCommand);
+                    this.send(this.userCommand);
                     if (this.passwordCommand !== this.userCommand) {
-                        this.sendRaw(this.passwordCommand);
+                        this.send(this.passwordCommand);
                     }
                 }
             };
@@ -154,26 +154,6 @@ class ArkadiaClient {
         try {
             this.socket.send(btoa(message + "\r\n"));
             // Only echo commands if we've received the first GMCP event
-            if (this.receivedFirstGmcp && message) {
-                Output.send("-> " + message);
-            }
-        } catch (error) {
-            console.error('Error sending message:', error);
-            this.emit('error', error);
-        }
-    }
-
-    /**
-     * Send a message without updating stored credentials
-     */
-    private sendRaw(message: string): void {
-        if (!this.socket || this.socket.readyState !== WebSocket.OPEN) {
-            console.error('WebSocket is not connected');
-            return;
-        }
-
-        try {
-            this.socket.send(btoa(message + "\r\n"));
             if (this.receivedFirstGmcp && message) {
                 Output.send("-> " + message);
             }
