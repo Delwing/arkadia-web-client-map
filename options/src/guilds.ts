769e27068e88676474eb807711a03371c4966f89
Piotr <delwing@gmail.com>
2025-07-17 10:43:56 +0200

    Sort guild list alphabetically

--- options/src/guilds.ts
+++ options/src/guilds.ts
@@ -1,8 +1,8 @@
 const guilds = [
-    "CKN", "ES", "SC", "KS", "KM", "OS",
-    "OHM", "SGW", "PE", "WKS", "LE", "KG",
-    "KGKS", "MC", "OK", "RA", "GL", "ZT",
-    "ZS", "ZH", "GP", "NPC"
+    "CKN", "ES", "GL", "GP", "KG", "KGKS",
+    "KM", "KS", "LE", "MC", "NPC", "OHM",
+    "OK", "OS", "PE", "RA", "SC", "SGW",
+    "WKS", "ZH", "ZS", "ZT",
 ];
 
 export default guilds;
