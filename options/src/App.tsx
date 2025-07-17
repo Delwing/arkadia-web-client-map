8a4bfdf50767de63852f61abfb6927e8b4833cec
Piotr <delwing@gmail.com>
2025-07-17 12:50:26 +0200

    move guild settings to separate tab (#381)

--- options/src/App.tsx
+++ options/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import {useState} from "react";
 import {Tabs, Tab} from 'react-bootstrap';
 import SettingsForm from "./Settings.tsx";
+import Guilds from "./Guilds";
 import Npc from "./Npc.tsx";
 import SettingsFile from "./SettingsFile";
 import Binds from "./Binds";
@@ -10,13 +11,16 @@ import Recordings from "./Recordings";
 
 
 function App() {
-    const [tab, setTab] = useState<'settings' | 'npc' | 'file' | 'binds' | 'scripts' | 'recordings'>('settings')
+    const [tab, setTab] = useState<'settings' | 'guilds' | 'npc' | 'file' | 'binds' | 'scripts' | 'recordings'>('settings')
 
     return (
         <div className="p-2 d-flex flex-column h-100">
             <Tabs activeKey={tab} onSelect={(k) => k && setTab(k as any)} className="mb-2" variant={"pills"}>
                 <Tab eventKey="settings" title="Ustawienia">
                     <SettingsForm />
+                </Tab>
+                <Tab eventKey="guilds" title="Gildie">
+                    <Guilds />
                 </Tab>
                 <Tab eventKey="npc" title="Odbiorcy paczek">
                     <Npc />
