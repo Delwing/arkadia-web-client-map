6828b9dac409a60ae4cdf360d07c9e35c0c58a5b
Piotr <delwing@gmail.com>
2025-07-19 03:23:00 +0200

    add take out for prettyContainers.ts (#413)
    * add take out for prettyContainers.ts
    
    * feat: enable container links after przejrzyj (#421)

--- client/src/stripAnsiCodes.ts
+++ client/src/stripAnsiCodes.ts
@@ -1,2 +1,2 @@
 export const stripAnsiCodes = (str: string): string =>
-    str.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, "");
+    str.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]|{click:\d+}/g, "");
